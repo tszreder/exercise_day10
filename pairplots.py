b36336d52667201961a5d03fef30ecfedac7b0b8
TSzreder <t.szreder@gmail.com>
2019-10-19 09:57:16 +0200

    new features for the plot

--- pairplots.py
+++ pairplots.py
@@ -5,8 +5,13 @@ sns.set(style='ticks')
 
 tips = sns.load_dataset('tips')
 
-g = sns.FacetGrid(tips, col='time')
+# g = sns.FacetGrid(tips, hue='time')
+#
+# g.map(plt.scatter,'total_bill', 'tip')
+# g.add_legend()
+
+g = sns.FacetGrid(tips, row='smoker', col='time', margin_titles=True)
+g.map(sns.regplot, 'size', 'total_bill', color='.3', fit_reg=False, x_jitter=.1)
 
 plt.show()
 
-print('Pairplots added')
